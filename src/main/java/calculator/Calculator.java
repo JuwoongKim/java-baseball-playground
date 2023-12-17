25f1d62bdf32f947140b5833fa5dcf90c1c69b8e
juwoong kim <wwong7247@gmail.com>
2023-12-17 10:05:03 +0900

    문자열 계산기 : 응집도를 위해 Operator를 Calculator 내부 enum 으로 변경

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,37 @@
 package calculator;
 
+import static calculator.exception.ErrorCode.*;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+import calculator.exception.ExpressionValidationException;
+
 public class Calculator {
+
+	public enum Operator {
+		ADD("+", (a, b) -> a + b),
+		SUBTRACT("-", (a, b) -> a - b),
+		MULTIPLY("*", (a, b) -> a * b),
+		DIVIDE("/", (a, b) -> a / b)
+		;
+
+		private String symbol;
+		private BiFunction<Long, Long, Long> operate;
+
+		Operator(String symbol, BiFunction<Long, Long, Long> operate) {
+			this.symbol = symbol;
+			this.operate = operate;
+		}
+
+		public static Long calculate(String operator, Long rightOperand, Long leftOperand) {
+			return Arrays.stream(Operator.values())
+				.filter(v -> v.symbol.equals(operator))
+				.findFirst()
+				.orElseThrow(() -> new ExpressionValidationException(INVALID_EXPRESSION.getMessage()))
+				.operate.apply(rightOperand, leftOperand);
+		}
+	}
 
 	public String calculate(Expression expression) {
 		String[] expressionElements = expression.getExpressionElements();
--- src/test/java/study/calculator/OperatorTest.java
+++ src/test/java/study/calculator/OperatorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import calculator.Operator;
+import calculator.Calculator;
 
 public class OperatorTest {
 
@@ -15,14 +15,14 @@ public class OperatorTest {
 	@ParameterizedTest
 	@CsvSource(value = {"+:10:5:15","-:10:5:5","*:10:5:50","/:10:5:2"}, delimiter = ':')
 	public void calculate(String operator, Long rightOperand, Long leftOperand, Long expectedResult) {
-		Long actualResult = Operator.calculate(operator,rightOperand,leftOperand);
+		Long actualResult = Calculator.Operator.calculate(operator,rightOperand,leftOperand);
 		assertThat(actualResult).isEqualTo(expectedResult);
 	}
 
 	@DisplayName("calculator 메서드 0으로 나눌 때 ArithmeticException 발생")
 	@Test
 	public void calculateWithArithmeticException(){
-		assertThatThrownBy(()->Operator.calculate("/", 10L, 0L))
+		assertThatThrownBy(()-> Calculator.Operator.calculate("/", 10L, 0L))
 			.isInstanceOf(ArithmeticException.class)
 			.hasMessageContaining("0으로 나누기");
 	}
