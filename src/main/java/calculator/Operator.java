7d647e19ab0d4c281310f4b59b8f3a0926550326
juwoong kim <wwong7247@gmail.com>
2023-12-17 09:57:47 +0900

    문자열 계산기 : Operator 연산 기능을 추상 메서드에서 함수형 인터페이스로 변경

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -3,52 +3,30 @@ package calculator;
 import static calculator.exception.ErrorCode.*;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 import calculator.exception.ExpressionValidationException;
 
 public enum Operator {
-	ADD("+") {
-		@Override
-		Long operate(Long a, Long b) {
-			return a + b;
-		}
-	},
-	SUBTRACT("-") {
-		@Override
-		Long operate(Long a, Long b) {
-			return a - b;
-		}
-	},
-	MULTIPLY("*") {
-		@Override
-		Long operate(Long a, Long b) {
-			return a * b;
-		}
-	},
-	DIVIDE("/") {
-		@Override
-		Long operate(Long a, Long b) {
-			return a / b;
-		}
-	};
+	ADD("+", (a, b) -> a + b),
+	SUBTRACT("-", (a, b) -> a - b),
+	MULTIPLY("*", (a, b) -> a * b),
+	DIVIDE("/", (a, b) -> a / b)
+	;
 
 	private String symbol;
+	private BiFunction<Long, Long, Long> operate;
 
-	Operator(String symbol) {
+	Operator(String symbol, BiFunction<Long, Long, Long> operate) {
 		this.symbol = symbol;
+		this.operate = operate;
 	}
-
-	public String getSymbol() {
-		return symbol;
-	}
-
-	abstract Long operate(Long a, Long b);
 
 	public static Long calculate(String operator, Long rightOperand, Long leftOperand) {
 		return Arrays.stream(Operator.values())
 			.filter(v -> v.symbol.equals(operator))
 			.findFirst()
 			.orElseThrow(() -> new ExpressionValidationException(INVALID_EXPRESSION.getMessage()))
-			.operate(rightOperand, leftOperand);
+			.operate.apply(rightOperand, leftOperand);
 	}
 }
